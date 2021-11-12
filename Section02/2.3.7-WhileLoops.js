93eac270f512c55a0cce593ed5605e7ea28f7c77
Jeff <jeffrey.edson@pm.me>
2021-11-11 19:47:54 -0600

    complete activity

--- Section02/2.3.7-WhileLoops.js
+++ Section02/2.3.7-WhileLoops.js
@@ -1,7 +1,12 @@
 // ? TO DO: create a while loop that TOTALS the numbers from 1 to 100
 // create a variable to represent the current number
-
+let num = 1;
 // create a variable to represent the current total
-
+let total = 0;
 // write a while loop that sums the numbers from 1 to 100
+while (num <= 100) {
+    total += num;
+    num++;
+}
 
+console.log(total);
